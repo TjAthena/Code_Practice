1e55539a2f960ff7519dae54e351a30c6764a222
Tjpradeep <107289568+TjAthena@users.noreply.github.com>
2024-05-28 11:56:37 +0530

    check a string is it palindrome or not

--- Maths_Problem_solving_prac/IDLE code Practice.py
+++ Maths_Problem_solving_prac/IDLE code Practice.py
@@ -1,1 +1,14 @@
-print("hello worl")
+#string palindrome 
+
+code = input("enter a string : ")
+a=code[0]
+b=code[-1]
+print(a)
+print(b)
+if(a==b):
+    print("number is palindrome ")
+else:
+    print("not palindrom")
+
+################################################################################################
+
